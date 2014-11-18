bbcd3dde61e9adf05e9f608b353d5275f29b88a1
Cătălin Mariș <alrraa@gmail.com>
2014-11-18 15:08:16 +0200

    ♻️  Make minor improvements

--- lib/ubuntu/install_applications.sh
+++ lib/ubuntu/install_applications.sh
@@ -36,7 +36,7 @@ declare -a APT_PACKAGES=(
 
 )
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 add_key() {
     wget -qO - "$1" | sudo apt-key add - &> /dev/null
@@ -46,6 +46,36 @@ add_key() {
 
 add_ppa() {
     sudo add-apt-repository -y ppa:"$1" &> /dev/null
+}
+
+add_software_sources() {
+
+    # Atom
+    [ $(cmd_exists "atom") -eq 1 ] \
+        && add_ppa "webupd8team/atom"
+
+    # Firefox Nightly
+    [ $(cmd_exists "firefox-trunk") -eq 1 ] \
+        && add_ppa "ubuntu-mozilla-daily/ppa"
+
+    # Google Chrome
+    [ $(cmd_exists "google-chrome") -eq 1 ] \
+        && add_key "https://dl-ssl.google.com/linux/linux_signing_key.pub" \
+        && add_source_list \
+                "http://dl.google.com/linux/deb/ stable main" \
+                "google-chrome.list"
+
+    # NodeJS
+    [ $(cmd_exists "node") -eq 1 ] \
+        && add_ppa "chris-lea/node.js"
+
+    # Opera & Opera Next
+    [ $(cmd_exists "opera") -eq 1 ] \
+        && add_key "http://deb.opera.com/archive.key" \
+        && add_source_list \
+                "http://deb.opera.com/opera/ stable non-free" \
+                "opera.list"
+
 }
 
 add_source_list() {
@@ -75,53 +105,20 @@ update_and_upgrade() {
     execute "sudo apt-get upgrade -qqy" "upgrade"
 }
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 install_applications() {
 
     local i="", tmp=""
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Add software sources
-
-    # Atom
-    [ $(cmd_exists "atom") -eq 1 ] \
-        && add_ppa "webupd8team/atom"
-
-    # Firefox Nightly
-    [ $(cmd_exists "firefox-trunk") -eq 1 ] \
-        && add_ppa "ubuntu-mozilla-daily/ppa"
-
-    # Google Chrome
-    [ $(cmd_exists "google-chrome") -eq 1 ] \
-        && add_key "https://dl-ssl.google.com/linux/linux_signing_key.pub" \
-        && add_source_list \
-                "http://dl.google.com/linux/deb/ stable main" \
-                "google-chrome.list"
-
-    # NodeJS
-    [ $(cmd_exists "node") -eq 1 ] \
-        && add_ppa "chris-lea/node.js"
-
-    # Opera & Opera Next
-    [ $(cmd_exists "opera") -eq 1 ] \
-        && add_key "http://deb.opera.com/archive.key" \
-        && add_source_list \
-                "http://deb.opera.com/opera/ stable non-free" \
-                "opera.list"
-
+    add_software_sources
     update_and_upgrade
 
     printf "\n"
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     for i in ${!APT_PACKAGES[*]}; do
         install_package "${APT_PACKAGES[$i]}"
     done
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     printf "\n"
 
