a5e5da9724d64caac2d9786c8f86245e4fd09f35
Cătălin Mariș <contact@catalinmaris.com>
2014-11-18 22:42:03 +0200

    📚 Update links
    * Fix broken links.
    * Use HTTPS where possible.

--- os/os_x/set_preferences.sh
+++ os/os_x/set_preferences.sh
@@ -89,7 +89,7 @@ set_finder_preferences() {
     defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
     # Enable `Secure Empty Trash`
-    # (Security of data on disk: http://www.youtube.com/watch?v=4SSSMi4X_mA)
+    # (Security of data on disk: https://www.youtube.com/watch?v=4SSSMi4X_mA)
     #defaults write com.apple.finder EmptyTrashSecurely -bool true
 
     # Search the current directory by default
