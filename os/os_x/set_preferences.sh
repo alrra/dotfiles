7b10cef357976664943ffc790aae93d5406db044
Cătălin Mariș <contact@catalinmaris.com>
2015-03-30 18:09:00 +0300

    🔧 Make Chrome expand print dialog on OS X
    Ref: https://github.com/mathiasbynens/dotfiles/pull/434#issuecomment-59643432

--- os/os_x/set_preferences.sh
+++ os/os_x/set_preferences.sh
@@ -16,6 +16,10 @@ set_chrome_and_chrome_canary_preferences() {
     defaults write com.google.Chrome DisablePrintPreview -bool true
     defaults write com.google.Chrome.canary DisablePrintPreview -bool true
 
+    # Expand the print dialog by default
+    defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
+    defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
+
 }
 
 # ----------------------------------------------------------------------
