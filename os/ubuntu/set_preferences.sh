a17902d2dc022bd341527faf2fdde3124e42001e
Cătălin Mariș <contact@catalinmaris.com>
2015-05-17 00:55:28 +0300

    🐛 Remove call to `fixubuntu.sh`
    * Remove call to the `fixubuntu.sh` script as it is no longer maintained.
      https://github.com/micahflee/fixubuntu/issues/44#issuecomment-102540300
    
    * Add custom privacy settings that I consider necessary for the Ubuntu
      versions that I occasionally use, namely, 14.04 LTS and 15.04.
    
    Thanks for the heads up @brendan-R!
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Fix: #7

--- os/ubuntu/set_preferences.sh
+++ os/ubuntu/set_preferences.sh
@@ -8,9 +8,13 @@ cd "$(dirname "${BASH_SOURCE}")" && source "../utils.sh"
 
 set_privacy_settings() {
 
-    # Fix privacy related issues present in Ubuntu 12.10-14.04
-    # https://fixubuntu.com/
-    return $(wget -qO - https://fixubuntu.com/fixubuntu.sh | bash > /dev/null; echo $?)
+    # Turn off "Remote Search" so that search
+    # terms in Dash don't get sent over the internet
+    gsettings set com.canonical.Unity.Lenses remote-content-search 'none'
+
+    # Disable Dash's "More suggestions" section
+    gsettings set com.canonical.Unity.ApplicationsLens display-available-apps false
+
 }
 
 # ----------------------------------------------------------------------
