9b9f880584cfe9fdf4ef9594b562dd483502e8e5
Cătălin Mariș <alrraa@gmail.com>
2015-06-07 18:04:33 +0300

    [osx] Execute brew cleanup after all installations

--- os/os_x/install_applications.sh
+++ os/os_x/install_applications.sh
@@ -125,7 +125,6 @@ main() {
 
         execute "brew update" "brew (update)"
         execute "brew upgrade --all" "brew (upgrade)"
-        execute "brew cleanup" "brew (cleanup)"
         printf "\n"
 
         brew_install "HOMEBREW_FORMULAE[@]"
@@ -145,6 +144,9 @@ main() {
 
         brew_tap "bramstein/webfonttools" \
             && brew_install "WEBFONT_TOOLS[@]"
+        printf "\n"
+
+        execute "brew cleanup" "brew (cleanup)"
         printf "\n"
 
     fi
