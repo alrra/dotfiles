46e1ec7294d3319ed987f176d35ec4a4305cf568
Cătălin Mariș <alrraa@gmail.com>
2015-06-26 11:01:25 +0300

    [os] Ensure nvm.sh is sourced before npm is called

--- os/install_npm_packages.sh
+++ os/install_npm_packages.sh
@@ -11,6 +11,15 @@ declare -r -a NPM_PACKAGES=(
 
 main() {
 
+    # Check if `NVM_DIR` is set, and if it's not, it's because
+    # `~/.bash.local` was not sourced, and therefore, npm won't
+    # be available (this happens when the `dotfiles` are set up
+    # for the first time)
+
+    if [ -z "$NVM_DIR" ]; then
+        source "$HOME/.bash.local"
+    fi
+
     # Check if `npm` is installed
     if [ $(cmd_exists "npm") -eq 1 ]; then
         print_error "npm is required, please install it!\n"
