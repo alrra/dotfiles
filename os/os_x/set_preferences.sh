4f585b2a0265626391bfac1f01ce38f0313f363e
Cătălin Mariș <alrraa@gmail.com>
2015-07-04 18:24:18 +0300

    [osx] Wipe also the stack icons from the Dock

--- os/os_x/set_preferences.sh
+++ os/os_x/set_preferences.sh
@@ -74,7 +74,8 @@ set_dock_preferences() {
     execute 'defaults write com.apple.dock mru-spaces -bool false' \
         'Do not automatically rearrange spaces based on most recent use'
 
-    execute 'defaults write com.apple.dock persistent-apps -array ""' \
+    execute 'defaults write com.apple.dock persistent-apps -array "" &&
+             defaults write com.apple.dock persistent-others -array ""' \
         'Wipe all app icons'
 
     execute 'defaults write com.apple.dock show-process-indicators -bool true' \
