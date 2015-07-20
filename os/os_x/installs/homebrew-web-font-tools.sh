db55badf400b7bcf2f9a5396acf3b6a1e8f5a4c7
Cătălin Mariș <contact@catalinmaris.com>
2015-07-20 14:50:44 +0300

    ♻️  Split `install-applications.sh` into more specialized scripts
    Make managing / installing particular groups of applications / CLTs
    easier by splitting `install-applications.sh` into more specialized
    scripts.

---
+++ os/os_x/installs/homebrew-web-font-tools.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE}")" \
+    && source '../../utils.sh' \
+    && source './utils.sh'
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main () {
+
+    # Webfont tools
+    # https://github.com/bramstein/homebrew-webfonttools
+
+    if cmd_exists 'brew' \
+        && brew_tap 'bramstein/webfonttools'; then
+
+        brew_install 'TTF/OTF → WOFF (Zopfli)' 'sfnt2woff-zopfli'
+        brew_install 'TTF/OTF → WOFF' 'sfnt2woff'
+        brew_install 'WOFF2' 'woff2'
+
+    fi
+
+}
+
+main
---
+++ os/os_x/installs/xcode.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE}")" \
+    && source '../../utils.sh' \
+    && source './utils.sh'
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main () {
+
+    if ! xcode-select -p &> /dev/null; then
+
+        # Prompt user to install the XCode Command Line Tools
+        xcode-select --install &> /dev/null
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        # Wait until the XCode Command Line Tools are installed
+        while ! xcode-select -p &> /dev/null; do
+            sleep 5
+        done
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        # Prompt user to agree to the terms of the Xcode license
+        # https://github.com/alrra/dotfiles/issues/10
+
+        sudo xcodebuild -license
+
+    fi
+
+    print_success 'XCode Command Line Tools\n'
+
+}
+
+main
