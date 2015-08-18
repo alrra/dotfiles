5b7694a40abaa6d1c444a19120414fb44f2186b1
Cătălin Mariș <alrraa@gmail.com>
2015-08-18 20:22:14 +0300

    ♻️  Split Ubuntu install script into more specialized scripts
    Make managing / installing particular groups of applications / CLTs
    easier by splitting `installs/main.sh` into more specialized scripts.

---
+++ os/ubuntu/installs/utils.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cd "$(dirname "$BASH_SOURCE")" \
+    && source '../../utils.sh'
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+add_key() {
+    wget -qO - "$1" | sudo apt-key add - &> /dev/null
+    #     │└─ write output to file
+    #     └─ don't show output
+}
+
+add_ppa() {
+    sudo add-apt-repository -y ppa:"$1" &> /dev/null
+}
+
+add_to_source_list() {
+    sudo sh -c "printf 'deb $1' >> '/etc/apt/sources.list.d/$2'"
+}
+
+install_package() {
+
+    declare -r PACKAGE="$2"
+    declare -r PACKAGE_READABLE_NAME="$1"
+
+    if ! package_is_installed "$PACKAGE"; then
+        execute "sudo apt-get install --allow-unauthenticated -qqy $PACKAGE" "$PACKAGE_READABLE_NAME"
+        #                                      suppress output ─┘│
+        #            assume "yes" as the answer to all prompts ──┘
+    else
+        print_success "$PACKAGE_READABLE_NAME"
+    fi
+
+}
+
+package_is_installed() {
+    dpkg -s "$1" &> /dev/null
+}
+
+update() {
+
+    # Resynchronize the package index files from their sources
+    execute 'sudo apt-get update -qqy' 'Update'
+
+}
+
+upgrade() {
+
+    # Install the newest versions of all packages installed
+    execute 'sudo apt-get upgrade -qqy' 'Upgrade'
+
+}
