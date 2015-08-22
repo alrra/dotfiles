a6b1c676f0c6c4bea883a1071772336aee904c11
Cătălin Mariș <contact@catalinmaris.com>
2015-08-22 18:02:15 +0300

    🔧 Make Vim's autocomplete better
    Improve Vim's autocomplete by installing the following plugins:
    
      * neosnippet
        https://github.com/Shougo/neosnippet.vim
    
      * neosnippet-snippet
        https://github.com/Shougo/neosnippet-snippets
    
      * tern_for_vim
        https://github.com/marijnh/tern_for_vim

--- os/install_vim_plugins.sh
+++ os/install_vim_plugins.sh
@@ -23,8 +23,15 @@ main() {
         && printf "\n" | vim +PluginInstall +qall 2> /dev/null
         #     └─ simulate the ENTER keypress for
         #        the case where there are warnings
-
     print_result $? 'Install Vim plugins'
+
+    printf '\n'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    cd ~/.vim/plugins/tern_for_vim \
+        && npm install &> /dev/null
+    print_result $? 'tern_for_vim (extra installations)'
 
 }
 
