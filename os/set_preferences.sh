fd3ade06d14e1388c9fe89c48e691f136e837c6a
Cătălin Mariș <alrraa@gmail.com>
2015-08-25 21:34:55 +0300

    [os] Make consistency related improvements

--- os/set_preferences.sh
+++ os/set_preferences.sh
@@ -9,9 +9,9 @@ main() {
 
     declare -r OS="$(get_os)"
 
-    if [ "$OS" == "osx" ]; then
+    if [ "$OS" == 'osx' ]; then
         ./os_x/preferences/main.sh
-    elif [ "$OS" == "ubuntu" ]; then
+    elif [ "$OS" == 'ubuntu' ]; then
         ./ubuntu/preferences/main.sh
     fi
 
