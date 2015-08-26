320d7f44f2e042c2297350d2386f2cd6c00ba987
Cătălin Mariș <alrraa@gmail.com>
2015-08-26 17:58:34 +0300

    [os] Simplify `create_symbolic_links.sh`

--- os/create_symbolic_links.sh
+++ os/create_symbolic_links.sh
@@ -36,27 +36,29 @@ main() {
     local sourceFile=''
     local targetFile=''
 
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     for i in ${FILES_TO_SYMLINK[@]}; do
 
         sourceFile="$(cd .. && pwd)/$i"
         targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
-        if [ -e "$targetFile" ]; then
-            if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
-
-                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
-                if answer_is_yes; then
-                    rm -rf "$targetFile"
-                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
-                else
-                    print_error "$targetFile → $sourceFile"
-                fi
-
-            else
-                print_success "$targetFile → $sourceFile"
-            fi
-        else
+        if [ ! -e "$targetFile" ]; then
             execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+            exit 0
+        fi
+
+        if [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
+            print_success "$targetFile → $sourceFile"
+            exit 0;
+        fi
+
+        ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+        if answer_is_yes; then
+            rm -rf "$targetFile"
+            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+        else
+            print_error "$targetFile → $sourceFile"
         fi
 
     done
