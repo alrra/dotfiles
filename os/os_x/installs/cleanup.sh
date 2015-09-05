35194712ba65efad562663cd8580691009afba57
Cătălin Mariș <alrraa@gmail.com>
2015-09-06 00:06:02 +0300

    [os|shell] Execute cleanup also for homebrew casks

--- os/os_x/installs/cleanup.sh
+++ os/os_x/installs/cleanup.sh
@@ -14,7 +14,10 @@ main() {
     # https://github.com/Homebrew/homebrew/blob/b311d1483fa434f6692ab8dddb0bfd876d01a668/share/doc/homebrew/FAQ.md#how-do-i-uninstall-old-versions-of-a-formula
 
     if cmd_exists 'brew'; then
+
         execute 'brew cleanup' 'brew (cleanup)'
+        execute 'brew cask cleanup' 'brew cask (cleanup)'
+
     fi
 
 }
