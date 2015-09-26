432515828c79aef0a5cebbcb4fef036226b2348b
Cătălin Mariș <contact@catalinmaris.com>
2015-09-26 20:33:42 +0300

    🐛 Fix "'xcodebuild' requires Xcode" error
    By default, the `xcode-select` developer directory points
    to `/Library/Developer/CommandLineTools`, but since it doesn't
    get (immediately?) updated to point to the appropriate directory
    from within `Xcode.app` once `Xcode` is installed, the following
    error will be raised when trying to install anything that require
    the `Xcode` CLTs:
    
      " xcode-select: error: tool 'xcodebuild' requires Xcode,
        but active developer directory '/Library/Developer/CommandLineTools'
        is a command line tools instance. "
    
    This commit fixes the issue described above by making
    the `xcode-select` developer directory point to the appropriate
    directory from within `Xcode.app` once Xcode is installed.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Ref: #13
    Fix: #13

--- os/os_x/installs/install_xcode.sh
+++ os/os_x/installs/install_xcode.sh
@@ -13,12 +13,23 @@ main() {
         # Prompt user to install the XCode Command Line Tools
         xcode-select --install &> /dev/null
 
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
         # Wait until the XCode Command Line Tools are installed
         while ! xcode-select -p &> /dev/null; do
             sleep 5
         done
 
         print_result $? 'Install XCode Command Line Tools'
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        # Point the `xcode-select` developer directory to
+        # the appropriate directory from within `Xcode.app`
+        # https://github.com/alrra/dotfiles/issues/13
+
+        sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+        print_result $? 'Make "xcode-select" developer directory point to Xcode'
 
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
