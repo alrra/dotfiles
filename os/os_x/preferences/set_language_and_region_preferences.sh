58ea68eabf6f144ef43ffba14b5a44594f7acf5e
Cătălin Mariș <alrraa@gmail.com>
2015-09-30 20:55:02 +0300

    [osx] Comment out some of the region preferences

--- os/os_x/preferences/set_language_and_region_preferences.sh
+++ os/os_x/preferences/set_language_and_region_preferences.sh
@@ -7,17 +7,17 @@ cd "$(dirname "$BASH_SOURCE")" \
 
 set_language_and_region_preferences() {
 
-    execute 'defaults write NSGlobalDomain AppleLanguages -array "en" "ro"' \
-        'Set language'
+    # execute 'defaults write NSGlobalDomain AppleLanguages -array "en"' \
+    #     'Set language'
 
-    execute 'defaults write NSGlobalDomain AppleLocale -string "en_RO@currency=EUR"' \
-        'Set locale'
+    # execute 'defaults write NSGlobalDomain AppleLocale -string "en_RO@currency=EUR"' \
+    #     'Set locale'
 
     execute 'defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"' \
         'Set measurement units'
 
-    execute 'sudo systemsetup -settimezone "Europe/Bucharest"' \
-        'Set timezone'
+    # execute 'sudo systemsetup -settimezone "Europe/Bucharest"' \
+    #     'Set timezone'
 
     execute 'defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false' \
         'Disable auto-correct'
