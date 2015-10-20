c96950fb1bc6f966372c3efdfb412becf3fd7dbd
Vítor Galvão <info@vitorgalvao.com>
2015-10-20 13:18:12 +0100

    ♻️  Make minor improvements
    Close: #14

--- os/os_x/installs/install_xcode.sh
+++ os/os_x/installs/install_xcode.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$BASH_SOURCE")" \
 
 main() {
 
-    if ! xcode-select -p &> /dev/null; then
+    if ! xcode-select --print-path &> /dev/null; then
 
         # Prompt user to install the XCode Command Line Tools
         xcode-select --install &> /dev/null
@@ -16,7 +16,7 @@ main() {
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
         # Wait until the XCode Command Line Tools are installed
-        while ! xcode-select -p &> /dev/null; do
+        until xcode-select --print-path &> /dev/null; do
             sleep 5
         done
 
