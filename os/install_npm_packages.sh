da368df7df65cb6a085af423a11e5ab1a2cb2c7d
Cătălin Mariș <contact@catalinmaris.com>
2015-11-04 22:54:15 +0200

    ✨ Install Babel CLI
    Ref: https://babeljs.io/docs/usage/cli/

--- os/install_npm_packages.sh
+++ os/install_npm_packages.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$BASH_SOURCE")" \
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 declare -r -a NPM_PACKAGES=(
-    'babel'
+    'babel-cli'
     'jshint'
     'svgo'
 )
