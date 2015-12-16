fca911d27e06dea661e18999bad6cf201d177474
Cătălin Mariș <contact@catalinmaris.com>
2015-12-16 20:45:11 +0200

    🔧 Remove unavailable simulators from Xcode

---
+++ os/os_x/preferences/set_xcode_preferences.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd "$(dirname "$BASH_SOURCE")" \
+    && source '../../utils.sh'
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+set_preferences() {
+
+    execute 'xcrun simctl delete unavailable' \
+        'Remove unavailable simulators'
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple '\n  Xcode\n\n'
+    set_preferences
+
+    killall 'Xcode' &> /dev/null
+
+}
+
+main
