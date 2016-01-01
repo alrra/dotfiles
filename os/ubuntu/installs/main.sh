fac31e97ece697c56c0ac1e546d754c9690ac543
Cătălin Mariș <alrraa@gmail.com>
2016-01-01 22:24:53 +0200

    ♻️  Move `install_packages.sh`'s content in `main.sh`

--- os/ubuntu/installs/main.sh
+++ os/ubuntu/installs/main.sh
@@ -9,14 +9,171 @@ cd "$(dirname "$BASH_SOURCE")" \
 main() {
 
     ./update_and_upgrade.sh
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     print_in_green '\n  ---\n\n'
 
-    ./install_packages.sh
+    # Tools for compiling/building software from source
+    install_package 'Build Essential' 'build-essential'
+
+    # GnuPG archive keys of the Debian archive
+    install_package 'GnuPG archive keys' 'debian-archive-keyring'
+
+    # Software which is not included by default
+    # in Ubuntu due to legal or copyright reasons
+    #install_package 'Ubuntu Restricted Extras' 'ubuntu-restricted-extras'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     print_in_green '\n  ---\n\n'
 
+    if ! package_is_installed 'atom'; then
+
+        add_ppa 'webupd8team/atom'
+        print_result $? "Atom (add PPA)"
+
+        update
+
+        install_package 'Atom' 'atom'
+
+    else
+        print_success 'Atom'
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if ! package_is_installed 'google-chrome-unstable'; then
+
+        add_key 'https://dl-ssl.google.com/linux/linux_signing_key.pub'
+        print_result $? "Chrome Canary (add key)"
+
+        add_to_source_list 'http://dl.google.com/linux/deb/ stable main' 'google-chrome.list'
+        print_result $? "Chrome Canary (add to package resource list)"
+
+        update
+
+        install_package 'Chrome Canary' 'google-chrome-unstable'
+
+    else
+        print_success 'Chrome Canary'
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'Chromium' 'chromium-browser'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'cURL' 'curl'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'Dropbox' 'nautilus-dropbox'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if ! package_is_installed 'firefox-trunk'; then
+
+        add_ppa 'ubuntu-mozilla-daily/ppa'
+        print_result $? "Firefox Nightly (add PPA)"
+
+        update
+
+        install_package 'Firefox Nightly' 'firefox-trunk'
+
+    else
+        print_success 'Firefox Nightly'
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'Flash' 'flashplugin-installer'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'GIMP' 'gimp'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'Git' 'git'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'GNOME Vim' 'vim-gnome'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'ImageMagick' 'imagemagick'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if ! package_is_installed 'opera'; then
+
+        add_key 'http://deb.opera.com/archive.key'
+        print_result $? "Opera (add key)"
+
+        add_to_source_list 'http://deb.opera.com/opera-stable/ stable non-free' 'opera.list'
+        print_result $? "Opera (add to package resource list)"
+
+        update
+
+    fi
+
+    if ! package_is_installed 'opera'; then
+        install_package 'Opera' 'opera'
+    else
+        print_success 'Opera'
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if ! package_is_installed 'opera-beta'; then
+        install_package 'Opera Beta' 'opera-beta'
+    else
+        print_success 'Opera Beta'
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if ! package_is_installed 'opera-developer'; then
+        install_package 'Opera Developer' 'opera-developer'
+    else
+        print_success 'Opera Developer'
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'tmux' 'tmux'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'Transmission' 'transmission'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'VirtualBox' 'virtualbox'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'VLC' 'vlc'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'xclip' 'xclip'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_package 'Zopfli' 'zopfli'
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_in_green '\n  ---\n\n'
     ./update_and_upgrade.sh
-    print_in_green '\n  ---\n\n'
 
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_in_green '\n  ---\n\n'
     ./cleanup.sh
 
 }
