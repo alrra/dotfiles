b253942728e56cd604635fa4f324ab47c289f5a4
Cătălin Mariș <contact@catalinmaris.com>
2016-01-04 18:53:48 +0200

    🔧 Reduce the install process' output on Ubuntu
    Change `main.sh` script so that for certain operations it only
    prints output if something failed.

--- os/ubuntu/installs/main.sh
+++ os/ubuntu/installs/main.sh
@@ -35,11 +35,11 @@ main() {
 
     if ! package_is_installed 'atom'; then
 
-        add_ppa 'webupd8team/atom'
-        print_result $? "Atom (add PPA)"
+        add_ppa 'webupd8team/atom' \
+            || print_error 'Atom (add PPA)'
 
-        update &> /dev/null
-        print_result $? "Atom (resynchronize package index files)"
+        update &> /dev/null \
+            || print_error 'Atom (resync package index files)'
 
     fi
 
@@ -49,14 +49,14 @@ main() {
 
     if ! package_is_installed 'google-chrome-unstable'; then
 
-        add_key 'https://dl-ssl.google.com/linux/linux_signing_key.pub'
-        print_result $? "Chrome Canary (add key)"
+        add_key 'https://dl-ssl.google.com/linux/linux_signing_key.pub' \
+            || print_error 'Chrome Canary (add key)'
 
-        add_to_source_list 'http://dl.google.com/linux/deb/ stable main' 'google-chrome.list'
-        print_result $? "Chrome Canary (add to package resource list)"
+        add_to_source_list 'http://dl.google.com/linux/deb/ stable main' 'google-chrome.list' \
+            || print_error 'Chrome Canary (add to package resource list)'
 
-        update &> /dev/null
-        print_result $? "Chrome Canary (resynchronize package index files)"
+        update &> /dev/null \
+            || print_error 'Chrome Canary (resynch package index files)'
 
     fi
 
@@ -78,11 +78,11 @@ main() {
 
     if ! package_is_installed 'firefox-trunk'; then
 
-        add_ppa 'ubuntu-mozilla-daily/ppa'
-        print_result $? "Firefox Nightly (add PPA)"
+        add_ppa 'ubuntu-mozilla-daily/ppa' \
+            || print_error 'Firefox Nightly (add PPA)'
 
-        update &> /dev/null
-        print_result $? "Firefox Nightly (resynchronize package index files)"
+        update &> /dev/null \
+            || print_error 'Firefox Nightly (resync package index files)' \
 
     fi
 
@@ -112,14 +112,14 @@ main() {
 
     if ! package_is_installed 'opera'; then
 
-        add_key 'http://deb.opera.com/archive.key'
-        print_result $? "Opera (add key)"
+        add_key 'http://deb.opera.com/archive.key' \
+            || print_error 'Opera (add key)'
 
-        add_to_source_list 'http://deb.opera.com/opera-stable/ stable non-free' 'opera.list'
-        print_result $? "Opera (add to package resource list)"
+        add_to_source_list 'http://deb.opera.com/opera-stable/ stable non-free' 'opera.list' \
+            || print_error 'Opera (add to package resource list)'
 
-        update &> /dev/null
-        print_result $? "Opera (resynchronize package index files)"
+        update &> /dev/null \
+            || print_error 'Opera (resync package index files)' \
 
     fi
 
