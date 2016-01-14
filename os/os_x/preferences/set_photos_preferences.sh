16c71216ddb22f8a778deea5cd6284163063cc98
Cătălin Mariș <alrraa@gmail.com>
2016-01-14 20:54:20 +0200

    🔧 Prevent Photos from opening automatically on OS X
    Thank you, @guerrero!
    
    Ref: https://github.com/mathiasbynens/dotfiles/pull/608

---
+++ os/os_x/preferences/set_photos_preferences.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd "$(dirname "$BASH_SOURCE")" \
+    && source '../../utils.sh'
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+set_preferences() {
+
+    execute 'defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true' \
+        'Prevent Photos from opening automatically when devices are plugged in'
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple '\n  Photos\n\n'
+    set_preferences
+
+    killall 'Photos' &> /dev/null
+
+}
+
+main
