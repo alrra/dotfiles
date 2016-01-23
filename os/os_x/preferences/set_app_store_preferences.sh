55662c8110cffe7d54ae309bb11e7f3c0a8be61f
Cătălin Mariș <contact@catalinmaris.com>
2016-01-23 18:47:55 +0200

    🔧 Enable the debug menu for App Store

---
+++ os/os_x/preferences/set_app_store_preferences.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd "$(dirname "$BASH_SOURCE")" \
+    && source '../../utils.sh'
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+set_preferences() {
+
+    execute 'defaults write com.apple.appstore ShowDebugMenu -bool true' \
+        'Enable debug menu'
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple '\n  App Store\n\n'
+    set_preferences
+
+    killall 'App Store' &> /dev/null
+
+}
+
+main
