bee3a8a562b5a5843771f633f40bd46ef867736a
Cătălin Mariș <alrraa@gmail.com>
2016-04-25 22:31:59 +0200

    🔧 Opt-out of Homebrew's analytics
    From https://github.com/Homebrew/brew/blob/0c95c60511cc4d85d28f66b58d51d85f8186d941/share/doc/homebrew/Analytics.md#opting-out:
    
      " ...to opt-out of Homebrew's analytics you may set
    
            `HOMEBREW_NO_ANALYTICS=1`
    
        in your environment or run
    
            `git config --file="$(brew --repository)/.git/config" --replace-all homebrew.analyticsdisabled true`
    
        which will prevent analytics from ever being sent
        when either of them have been set. "

--- os/os_x/installs/install_homebrew.sh
+++ os/os_x/installs/install_homebrew.sh
@@ -8,12 +8,36 @@ cd "$(dirname "$BASH_SOURCE")" \
 
 main() {
 
+    local HOMEBREW_GIT_CONFIG_FILE=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     if ! cmd_exists "brew"; then
         printf "\n" | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &> /dev/null
         #  └─ simulate the ENTER keypress
     fi
 
     print_result $? "Homebrew"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # If something when wrong so far, don't continue any further
+
+    [ $? -ne 0 ] \
+        && exit 0
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Opt-out of Homebrew's analytics
+    # https://github.com/Homebrew/brew/blob/0c95c60511cc4d85d28f66b58d51d85f8186d941/share/doc/homebrew/Analytics.md#opting-out
+
+    HOMEBREW_GIT_CONFIG_FILE="$(brew --repository)/.git/config"
+
+    if [ "$(git config --file="$HOMEBREW_GIT_CONFIG_FILE" --get homebrew.analyticsdisabled)" != "true" ]; then
+        git config --file="$HOMEBREW_GIT_CONFIG_FILE" --replace-all homebrew.analyticsdisabled true &> /dev/null
+    fi
+
+    print_result $? "Homebrew (opt-out of analytics)"
 
 }
 
