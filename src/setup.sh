07be2cd3ae0a978eb8e0e91066bca9bc249e9bf3
Cătălin Mariș <alrraa@gmail.com>
2016-05-03 01:07:27 +0300

    Move `dotfiles` specific code in `src/`

--- src/setup.sh
+++ src/setup.sh
@@ -4,7 +4,7 @@ declare -r GITHUB_REPOSITORY="alrra/dotfiles"
 
 declare -r DOTFILES_ORIGIN="git@github.com:$GITHUB_REPOSITORY.git"
 declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/master"
-declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/master/os/utils.sh"
+declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/master/src/os/utils.sh"
 
 declare dotfilesDirectory="$HOME/projects/dotfiles"
 
@@ -101,7 +101,7 @@ download_dotfiles() {
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    cd "$dotfilesDirectory" \
+    cd "$dotfilesDirectory/src" \
         || return 1
 
 }
