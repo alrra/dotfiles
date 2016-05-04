9cc3c6c9d68f42ae9f4b68070df50e06516f326b
Cătălin Mariș <contact@catalinmaris.com>
2016-05-04 19:48:27 +0300

    ✅ Don't suppress output from ShellCheck

--- test/run_shellcheck.sh
+++ test/run_shellcheck.sh
@@ -16,6 +16,11 @@ main() {
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+    # ' At first you're like "shellcheck is awesome" but then you're
+    #   like "wtf are we still using bash" '
+    #
+    #  (from: https://twitter.com/astarasikov/status/568825996532707330)
+
     find \
         ../test \
         ../src/os \
@@ -33,7 +38,7 @@ main() {
                 -e SC1090 \
                 -e SC1091 \
                 -e SC2155 \
-        {} + &> /dev/null
+        {} +
 
     print_result $? "Run code through ShellCheck"
 
