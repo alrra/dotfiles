268d30eb88f37f5385141ecef71e0a512d43c24c
Cătălin Mariș <alrraa@gmail.com>
2016-05-08 15:57:39 +0300

    ✨ Add option to skip the setup process prompts
    Add `-y/--yes` option to allow certain parts of the dotfiles setup
    process to be automatically executed, while the others (mainly the
    ones that require more then a yes/no answer) to be skipped.
    
    When used, this option will remove all need for human interaction,
    and thus, allow to more easily test the dotfiles.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Ref: #24

--- src/os/create_symbolic_links.sh
+++ src/os/create_symbolic_links.sh
@@ -36,6 +36,17 @@ main() {
     local i=""
     local sourceFile=""
     local targetFile=""
+    local skipQuestions=false
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    while :; do
+        case $1 in
+            -y|--yes) skipQuestions=true; break;;
+                   *) break;;
+        esac
+        shift 1
+    done
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -44,22 +55,28 @@ main() {
         sourceFile="$(cd .. && pwd)/$i"
         targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
-        if [ ! -e "$targetFile" ]; then
+        if [ ! -e "$targetFile" ] || $skipQuestions; then
             execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
         elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
             print_success "$targetFile → $sourceFile"
         else
-            ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
-            if answer_is_yes; then
-                rm -rf "$targetFile"
-                execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
-            else
-                print_error "$targetFile → $sourceFile"
+
+            if ! $skipQuestions; then
+
+                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                if answer_is_yes; then
+                    rm -rf "$targetFile"
+                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+                else
+                    print_error "$targetFile → $sourceFile"
+                fi
+
             fi
+
         fi
 
     done
 
 }
 
-main
+main "$@"
