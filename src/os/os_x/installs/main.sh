e436d18893d5e32af1d4c6bc7d4321208af898f4
Cătălin Mariș <contact@catalinmaris.com>
2016-05-27 22:18:34 +0200

    ♻️  Refactor OS X install scripts

--- src/os/os_x/installs/main.sh
+++ src/os/os_x/installs/main.sh
@@ -6,152 +6,26 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-main() {
+./install_xcode.sh
 
-    ./install_xcode.sh
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    print_in_green "\n  ---\n\n"
+print_in_green "\n  ---\n\n"
+./install_homebrew.sh
+brew_update
+brew_upgrade
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    ./install_homebrew.sh
-    ./update_and_upgrade.sh
+print_in_green "\n  ---\n\n"
+./install_bash.sh
 
-    print_in_green "\n  ---\n\n"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+print_in_green "\n  ---\n\n"
+./install_apps.sh
 
-    brew_install "Bash" "bash"
-    brew_install "Bash Completion 2" "bash-completion2" "homebrew/versions"
-    ./change_default_bash_version.sh
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    print_in_green "\n  ---\n\n"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Android File Transfer" "android-file-transfer" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Chrome" "google-chrome" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Chrome Canary" "google-chrome-canary" "caskroom/versions" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Chromium" "chromium" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "FFmpeg" "ffmpeg"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Firefox" "firefox" "caskroom/cask" "cask"
-    brew_install "Firefox Developer" "firefoxdeveloperedition" "caskroom/versions" "cask"
-    brew_install "Firefox Nightly" "firefoxnightly" "caskroom/versions" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Flash" "flash" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "GIMP" "lisanet-gimp" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Git" "git"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "ImageAlpha" "imagealpha" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "ImageMagick" "imagemagick --with-webp"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "ImageOptim" "imageoptim" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "LICEcap" "licecap" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "LibreOffice" "libreoffice" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Opera" "opera" "caskroom/cask" "cask"
-    brew_install "Opera Beta" "opera-beta" "caskroom/versions" "cask"
-    brew_install "Opera Developer" "opera-developer" "caskroom/versions" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # `Safari Technology Preview` requires OS X 10.11.4 or later
-    # https://github.com/alrra/dotfiles/issues/26
-
-    if is_supported_version "$(get_os_version)" "10.11.4"; then
-        brew_install "Safari Technology Preview" "safari-technology-preview" "caskroom/versions" "cask"
-    fi
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "ShellCheck" "shellcheck"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Spectacle" "spectacle" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "tmux" "tmux"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Transmission" "transmission" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Unarchiver" "the-unarchiver" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Vim" "vim --override-system-vi"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "VirtualBox" "virtualbox" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "VLC" "vlc" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Web Font Tools: TTF/OTF → WOFF (Zopfli)" "sfnt2woff-zopfli" "bramstein/webfonttools"
-    brew_install "Web Font Tools: TTF/OTF → WOFF" "sfnt2woff" "bramstein/webfonttools"
-    brew_install "Web Font Tools: WOFF2" "woff2" "bramstein/webfonttools"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "WebKit" "webkit-nightly" "caskroom/versions" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "Zopfli" "zopfli"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    print_in_green "\n  ---\n\n"
-
-    ./cleanup.sh
-
-}
-
-main
+print_in_green "\n  ---\n\n"
+brew_cleanup
