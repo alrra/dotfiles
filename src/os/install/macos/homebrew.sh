e2d507af3f3d56673e65edcaeda49959edbb6bf8
Cătălin Mariș <alrraa@gmail.com>
2016-06-22 23:38:08 +0300

    [macos] Refactor `homebrew.sh`

--- src/os/install/macos/homebrew.sh
+++ src/os/install/macos/homebrew.sh
@@ -6,42 +6,70 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-main() {
+get_homebrew_git_config_file_path() {
 
-    local homebrewGitConfigFilePath=""
+    local path=""
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    path="$(brew --repository 2> /dev/null)/.git/config"
+
+    if [ $? -eq 0 ]; then
+        printf "%s" "$path"
+        return 0
+    else
+        print_error "brew (get config file path)"
+        return 1
+    fi
+
+}
+
+install_homebrew() {
 
     if ! cmd_exists "brew"; then
         printf "\n" | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &> /dev/null
         #  └─ simulate the ENTER keypress
     fi
 
-    print_result $? "brew" \
-        || return 1
+    print_result $? "brew"
+
+}
+
+opt_out_of_analytics() {
+
+    local path=""
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    # Try to get the path of the `Homebrew` git config file,
-    # and if something fails, don't continue any further
+    # Try to get the path of the `Homebrew` git config file
 
-    homebrewGitConfigFilePath="$(brew --repository 2> /dev/null)/.git/config"
-
-    if [ $? -ne 0 ]; then
-        print_error "brew (get config file path)"
-        return 1
-    fi
+    path="$(get_homebrew_git_config_file_path)" \
+        || return 1
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     # Opt-out of Homebrew's analytics
     # https://github.com/Homebrew/brew/blob/0c95c60511cc4d85d28f66b58d51d85f8186d941/share/doc/homebrew/Analytics.md#opting-out
 
-    if [ "$(git config --file="$homebrewGitConfigFilePath" --get homebrew.analyticsdisabled)" != "true" ]; then
-        git config --file="$homebrewGitConfigFilePath" --replace-all homebrew.analyticsdisabled true &> /dev/null
+    if [ "$(git config --file="$path" --get homebrew.analyticsdisabled)" != "true" ]; then
+        git config --file="$path" --replace-all homebrew.analyticsdisabled true &> /dev/null
     fi
 
     print_result $? "brew (opt-out of analytics)"
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_info " Homebrew"
+
+    install_homebrew
+    opt_out_of_analytics
+
+    brew_update
+    brew_upgrade
 
 }
 
