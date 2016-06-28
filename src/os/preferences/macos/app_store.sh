5835b85fa7b1cd26f90f5c79e1f0b00552839a00
Cătălin Mariș <alrraa@gmail.com>
2016-06-28 21:57:54 +0300

    🔧 Add App Store preferences
    Thank you, @kdeldycke!

--- src/os/preferences/macos/app_store.sh
+++ src/os/preferences/macos/app_store.sh
@@ -10,4 +10,16 @@ print_info " App Store"
 execute "defaults write com.apple.appstore ShowDebugMenu -bool true" \
     "Enable debug menu"
 
+execute "defaults write com.apple.commerce AutoUpdate -bool true" \
+    "Turn on auto-update"
+
+execute "defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true" \
+    "Enable automatic update check"
+
+execute "defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1" \
+    "Download newly available updates in background"
+
+execute "defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1" \
+    "Install System data files and security updates"
+
 killall "App Store" &> /dev/null
