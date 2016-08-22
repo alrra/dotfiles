c201bbf8623f4e3b4cb64967e8a58bf5c85585f7
Cătălin Mariș <alrraa@gmail.com>
2016-08-22 17:42:19 +0200

    [os] Move GitHub SSH specific code in own file

---
+++ src/os/set_github_ssh_key.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+add_ssh_configs() {
+
+    printf "%s\n" \
+        "Host github.com" \
+        "  IdentityFile $1" \
+        "  LogLevel ERROR" >> ~/.ssh/config
+
+    print_result $? "Add SSH configs"
+
+}
+
+copy_public_ssh_key_to_clipboard () {
+
+    if cmd_exists "pbcopy"; then
+
+        pbcopy < "$1"
+        print_result $? "Copy public SSH key to clipboard"
+
+    elif cmd_exists "xclip"; then
+
+        xclip -selection clip < "$1"
+        print_result $? "Copy public SSH key to clipboard"
+
+    else
+        print_warning "Please copy the public SSH key ($1) to clipboard"
+    fi
+
+}
+
+generate_ssh_keys() {
+
+    ask "Please provide an email address (email): " && printf "\n"
+    ssh-keygen -t rsa -b 4096 -C "$(get_answer)" -f "$1"
+
+    print_result $? "Generate SSH keys"
+
+}
+
+open_github_ssh_page() {
+
+    declare -r GITHUB_SSH_URL="https://github.com/settings/ssh"
+
+    # The order of the following checks matters
+    # as on Ubuntu there is also a utility called `open`.
+
+    if cmd_exists "xdg-open"; then
+        xdg-open "$GITHUB_SSH_URL"
+    elif cmd_exists "open"; then
+        open "$GITHUB_SSH_URL"
+    else
+        print_warning "Please add the public SSH key to GitHub ($GITHUB_SSH_URL)"
+    fi
+
+}
+
+set_github_ssh_key() {
+
+    local sshKeyFileName="$HOME/.ssh/github"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # If there is already a file with that
+    # name, generate another, unique, file name.
+
+    if [ -f "$sshKeyFileName" ]; then
+        sshKeyFileName="$(mktemp -u "$HOME/.ssh/github_XXXXX")"
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    generate_ssh_keys "$sshKeyFileName"
+    add_ssh_configs "$sshKeyFileName"
+    copy_public_ssh_key_to_clipboard "${sshKeyFileName}.pub"
+    open_github_ssh_page
+    test_ssh_connection \
+        && rm "${sshKeyFileName}.pub"
+
+}
+
+test_ssh_connection() {
+
+    while true; do
+
+        ssh -T git@github.com &> /dev/null
+        [ $? -eq 1 ] && break
+
+        sleep 5
+
+    done
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple " Set up GitHub SSH keys\n\n"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if ! is_git_repository; then
+        print_error "Not a Git repository"
+        exit 1
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    ssh -T git@github.com &> /dev/null
+
+    if [ $? -ne 1 ]; then
+        set_github_ssh_key
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_result $? "Set up GitHub SSH keys"
+
+}
+
+main
--- src/os/update_content.sh
+++ src/os/update_content.sh
@@ -5,116 +5,19 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-add_ssh_configs() {
-
-    printf "%s\n" \
-        "Host github.com" \
-        "  IdentityFile $1" \
-        "  LogLevel ERROR" >> ~/.ssh/config
-
-    print_result $? "Add SSH configs"
-
-}
-
-copy_public_ssh_key_to_clipboard () {
-
-    if cmd_exists "pbcopy"; then
-
-        pbcopy < "$1"
-        print_result $? "Copy public SSH key to clipboard"
-
-    elif cmd_exists "xclip"; then
-
-        xclip -selection clip < "$1"
-        print_result $? "Copy public SSH key to clipboard"
-
-    else
-        print_warning "Please copy the public SSH key ($1) to clipboard"
-    fi
-
-}
-
-generate_ssh_keys() {
-
-    ask "Please provide an email address (email): " && printf "\n"
-    ssh-keygen -t rsa -b 4096 -C "$(get_answer)" -f "$1"
-
-    print_result $? "Generate SSH keys"
-
-}
-
-open_github_ssh_page() {
-
-    declare -r GITHUB_SSH_URL="https://github.com/settings/ssh"
-
-    # The order of the following checks matters
-    # as on Ubuntu there is also a utility called `open`.
-
-    if cmd_exists "xdg-open"; then
-        xdg-open "$GITHUB_SSH_URL"
-    elif cmd_exists "open"; then
-        open "$GITHUB_SSH_URL"
-    else
-        print_warning "Please add the public SSH key to GitHub ($GITHUB_SSH_URL)"
-    fi
-
-}
-
-set_github_ssh_key() {
-
-    local sshKeyFileName="$HOME/.ssh/github"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # If there is already a file with that
-    # name, generate another, unique, file name.
-
-    if [ -f "$sshKeyFileName" ]; then
-        sshKeyFileName="$(mktemp -u "$HOME/.ssh/github_XXXXX")"
-    fi
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    print_in_purple " Set up the SSH keys\n"
-
-    generate_ssh_keys "$sshKeyFileName"
-    add_ssh_configs "$sshKeyFileName"
-    copy_public_ssh_key_to_clipboard "${sshKeyFileName}.pub"
-    open_github_ssh_page
-    test_ssh_connection \
-        && rm "${sshKeyFileName}.pub"
-
-}
-
-test_ssh_connection() {
-
-    while true; do
-
-        ssh -T git@github.com &> /dev/null
-        [ $? -eq 1 ] && break
-
-        sleep 5
-
-    done
-
-    print_result $? "Set up the SSH keys"
-
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 main() {
 
-    if is_git_repository; then
+    ssh -T git@github.com &> /dev/null
 
-        ssh -T git@github.com &> /dev/null
+    if [ $? -ne 2 ]; then
+        ./set_github_ssh_key.sh
+    fi
 
-        [ $? -ne 1 ] \
-            && set_github_ssh_key
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    if [ $? -eq 0 ]; then
 
-        # Update content and remove untracked files.
+        print_in_purple "\n Update content\n\n"
 
         execute \
             "git fetch --all \
