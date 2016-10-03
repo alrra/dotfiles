4b5701365015553f1e5efd191f4cc8e8b22eb4c2
Cătălin Mariș <alrraa@gmail.com>
2016-10-03 11:24:33 +0300

    [os] Add script to create local config files

---
+++ src/os/create_local_config_files.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+create_bash_local() {
+
+    declare -r FILE_PATH="$HOME/.bash.local"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if [ ! -e "$FILE_PATH" ] || [ -z "$FILE_PATH" ]; then
+        printf "%s\n\n" "#!/bin/bash" >> "$FILE_PATH"
+    fi
+
+    print_result $? "$FILE_PATH"
+
+}
+
+create_gitconfig_local() {
+
+    declare -r FILE_PATH="$HOME/.gitconfig.local"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if [ ! -e "$FILE_PATH" ] || [ -z "$FILE_PATH" ]; then
+
+        printf "%s\n" \
+            "[user]" \
+            "    name = " \
+            "    email = " \
+            "    signingkey = " \
+        >> "$FILE_PATH"
+    fi
+
+    print_result $? "$FILE_PATH"
+
+}
+
+create_vimrc_local() {
+
+    declare -r FILE_PATH="$HOME/.vimrc.local"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if [ ! -e "$FILE_PATH" ]; then
+        printf "" >> "$FILE_PATH"
+    fi
+
+    print_result $? "$FILE_PATH"
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "\n • Create local config files\n\n"
+
+    create_bash_local
+    create_gitconfig_local
+    create_vimrc_local
+
+}
+
+main
