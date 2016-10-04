eef3f095a4a856f080bb1db05d4d6f1a0cfcde99
Cătălin Mariș <alrraa@gmail.com>
2016-10-04 13:41:15 +0300

    [macos] Fix TextEdit's plain text related config

--- src/os/preferences/macos/textedit.sh
+++ src/os/preferences/macos/textedit.sh
@@ -11,7 +11,7 @@ execute "defaults write com.apple.TextEdit PlainTextEncoding -int 4 && \
          defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4" \
     "Open and save files as UTF-8 encoded"
 
-execute "defaults write com.apple.TextEdit RichText -" \
+execute "defaults write com.apple.TextEdit RichText 0" \
     "Use plain text mode for new documents"
 
 killall "TextEdit" &> /dev/null
