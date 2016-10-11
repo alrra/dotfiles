ef7840d7fdea289879deabfd383e6d126534cfa3
Cătălin Mariș <contact@catalinmaris.com>
2016-10-11 21:46:17 +0300

    ♻️  Simplify and improve `show_spinner` shell function

--- src/os/utils.sh
+++ src/os/utils.sh
@@ -283,100 +283,61 @@ show_spinner() {
     local -r MSG="$3"
     local -r PID="$1"
 
-    local frameText=""
-    local frameTextLenght=0
     local i=0
-    local j=0
-    local numberOfLinesToBeCleared=0
-    local terminalWindowWidth=0
+    local frameText=""
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    # For commands that require sudo, if the password needs to be
-    # provided, wait for the user to provide it before showing the
-    # actual spinner.
-    #
-    # (this is kinda hacky, but yeah...)
+    # Note: In order for the Travis CI site to display
+    # things correctly, it needs special treatment, hence,
+    # the "is Travis CI?" checks.
 
-    if printf "%s" "$CMDS" | grep "sudo" &>/dev/null; then
-        while kill -0 "$PID" &>/dev/null \
-                && ! sudo -n true &> /dev/null; do
-            sleep 0.2
-        done
+    if [ "$TRAVIS" != "true" ]; then
+
+        # Provide more space so that the text hopefully
+        # doesn't reach the bottom line of the terminal window.
+        #
+        # This is a workaround for escape sequences not tracking
+        # the buffer position (accounting for scrolling).
+        #
+        # See also: https://unix.stackexchange.com/a/278888
+
+        printf "\n\n\n"
+        tput cuu 3
+
+        tput sc
+
     fi
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     # Display spinner while the commands are being executed.
 
     while kill -0 "$PID" &>/dev/null; do
 
         frameText="   [${FRAMES:i++%NUMBER_OR_FRAMES:1}] $MSG"
-        numberOfLinesToBeCleared=1
 
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
         # Print frame text.
 
-        printf "%s" "$frameText"
+        if [ "$TRAVIS" != "true" ]; then
+            printf "%s\n" "$frameText"
+        else
+            printf "%s" "$frameText"
+        fi
+
         sleep 0.2
 
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
         # Clear frame text.
 
-        # Notes:
-        #
-        #  * After the content surpasses the initial terminal height
-        #    (the content forces the scroll), `tput sc` (save the cursor
-        #    position) and `tput rc` (restore the cursor position) will
-        #    no longer be reliable.
-        #
-        # * `tput ed` (clear to end of screen) seems to also not always
-        #    be reliable.
-        #
-        # So, in order to work around the shortcomings described above,
-        # the clearing of the previous printed content will have to be
-        # done "manually".
-
-        # The content may not fit into a single line so there is a
-        # need to determine on how many lines it is printed on and
-        # clear every single one of those lines.
-
-        terminalWindowWidth=$(tput cols)
-        frameTextLenght=${#frameText}
-
-        if [ "$terminalWindowWidth" -lt "$frameTextLenght" ]; then
-            numberOfLinesToBeCleared=$(( numberOfLinesToBeCleared + ( frameTextLenght / terminalWindowWidth ) ))
-        fi
-
-        for j in $(seq 1 $numberOfLinesToBeCleared); do
-
-            # Clear current line.
-
-            tput el     # Clear to end of line.
-            tput el1    # Clear to beginning of line.
-
-            # - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-            # The following line is just so that things look ok on
-            # the Travis CI site. The line isn't really needed, but
-            # also it doesn't do any harm. However, without it all
-            # the frames of the spinner will just be displayed one
-            # after the other in a single line.
-
+        if [ "$TRAVIS" != "true" ]; then
+            tput rc
+        else
             printf "\r"
-
-            # - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-            # Move up one line if the line containing the starting
-            # position of the content has not been reached.
-
-            if [ "$j" -lt "$numberOfLinesToBeCleared" ]; then
-                tput cuu1
-            fi
-
-        done
+        fi
 
     done
 
