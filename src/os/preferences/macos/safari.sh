a4190b96ce7f1e7aae98347442a8eb33a2037920
Cătălin Mariș <alrraa@gmail.com>
2016-10-17 14:44:29 +0300

    [macos] Use `-g` instead of `NSGlobalDomain`
    From https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man1/defaults.1.html:
    
     " -g | -globalDomain | NSGlobalDomain
    
       Specify the global domain. '-g' and '-globalDomain'
       may be used as synonyms for NSGlobalDomain. "
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Thanks, @Menubalk!

--- src/os/preferences/macos/safari.sh
+++ src/os/preferences/macos/safari.sh
@@ -37,7 +37,7 @@ execute "defaults write com.apple.Safari SuppressSearchSuggestions -bool true &&
          defaults write com.apple.Safari UniversalSearchEnabled -bool false" \
     "Don’t send search queries to Apple"
 
-execute "defaults write NSGlobalDomain WebKitDeveloperExtras -bool true" \
+execute "defaults write -g WebKitDeveloperExtras -bool true" \
     "Add a context menu item for showing the 'Web Inspector' in web views"
 
 killall "Safari" &> /dev/null
