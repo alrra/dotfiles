bb52d6216e9612a155a05e3152b713ac145ce0e9
Cătălin Mariș <contact@catalinmaris.com>
2016-11-01 09:40:14 +0200

    ✨ Add Instant Markdown Vim plugin
    Ref:
    
      * https://github.com/suan/vim-instant-markdown
      * https://github.com/suan/instant-markdown-d

--- src/os/install/npm.sh
+++ src/os/install/npm.sh
@@ -22,6 +22,7 @@ main() {
 
     install_npm_package "npm (update)" "npm"
     printf "\n"
+    install_npm_package "!nstant-markdown-d" "instant-markdown-d"
     install_npm_package "Babel" "babel-cli"
     install_npm_package "JSHint" "jshint"
     install_npm_package "Yarn"  "yarn"
