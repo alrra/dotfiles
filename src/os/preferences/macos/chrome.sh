96d4982a08a337972195660ce597471f8e6adcac
Christopher Moura <christophermoura@users.noreply.github.com>
2017-02-10 00:28:44 -0200

    Update chrome.sh
    Comment the 'use system-native print preview dialog' instruction.

--- src/os/preferences/macos/chrome.sh
+++ src/os/preferences/macos/chrome.sh
@@ -13,7 +13,7 @@ execute "defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -b
 execute "defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true" \
     "Expand print dialog by default"
 
-execute "defaults write com.google.Chrome DisablePrintPreview -bool true" \
-    "Use system-native print preview dialog"
+#execute "defaults write com.google.Chrome DisablePrintPreview -bool true" \
+#    "Use system-native print preview dialog"
 
 killall "Google Chrome" &> /dev/null
