2c1d0dbbe065588d59baf7b54d1baa2f888fef04
Cătălin Mariș <contact@catalinmaris.com>
2017-03-07 22:05:30 +0200

    💥 Remove Yarn
    Remove Yarn from the install list as I don't use it that often.

--- src/os/install/ubuntu/main.sh
+++ src/os/install/ubuntu/main.sh
@@ -145,23 +145,6 @@ install_apps() {
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    if ! package_is_installed "yarn"; then
-
-        add_key "https://dl.yarnpkg.com/debian/pubkey.gpg" \
-            || print_error "Yarn (add key)"
-
-        add_to_source_list "https://dl.yarnpkg.com/debian/ stable main" "yarn.list" \
-            || print_error "Yarn (add to package resource list)"
-
-        update &> /dev/null \
-            || print_error "Yarn (resync package index files)"
-
-    fi
-
-    install_package "Yarn" "yarn"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     install_package "Zopfli" "zopfli"
 
 }
