a68ebe6c8bdf1b7fdec9695e44a4e8d52c4df668
Cătălin Mariș <alrraa@gmail.com>
2018-03-17 14:44:28 -0700

    ✨ Install Yarn

--- src/os/install/ubuntu/misc_tools.sh
+++ src/os/install/ubuntu/misc_tools.sh
@@ -11,3 +11,21 @@ print_in_purple "\n   Miscellaneous Tools\n\n"
 install_package "cURL" "curl"
 install_package "ShellCheck" "shellcheck"
 install_package "xclip" "xclip"
+
+if [ -d "$HOME/.nvm" ]; then
+
+    if ! package_is_installed "yarn"; then
+
+        add_key "https://dl.yarnpkg.com/debian/pubkey.gpg" \
+            || print_error "Yarn (add key)"
+
+        add_to_source_list "https://dl.yarnpkg.com/debian/ stable main" "yarn.list" \
+            || print_error "Yarn (add to package resource list)"
+
+        update &> /dev/null \
+            || print_error "Yarn (resync package index files)"
+
+    fi
+
+    install_package "Yarn" "yarn" "--no-install-recommends"
+fi
