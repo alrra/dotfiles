a6972f548fa877f31a8194506a64520f6ecaa303
Cătălin Mariș <alrraa@gmail.com>
2018-03-17 14:44:28 -0700

    [os] Add `Yarn` to the install list

--- src/os/install/macos/misc_tools.sh
+++ src/os/install/macos/misc_tools.sh
@@ -10,3 +10,7 @@ print_in_purple "\n   Miscellaneous Tools\n\n"
 
 brew_install "ShellCheck" "shellcheck"
 brew_install "Visual Studio Code" "visual-studio-code" "caskroom/cask" "cask"
+
+if [ -d "$HOME/.nvm" ]; then
+    brew_install "Yarn" "yarn" "" "" "--without-node"
+fi
