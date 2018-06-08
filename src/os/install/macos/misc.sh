3e0a7d00d57217288579694ccae77ef4259235b7
Cătălin Mariș <alrraa@gmail.com>
2018-06-08 08:40:59 -0700

    ✨ Add more macOS tools to the install list

--- src/os/install/macos/misc.sh
+++ src/os/install/macos/misc.sh
@@ -9,6 +9,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Miscellaneous\n\n"
 
 brew_install "Android File Transfer" "android-file-transfer" "caskroom/cask" "cask"
+brew_install "eyeD3" "eye-d3"
 brew_install "Spectacle" "spectacle" "caskroom/cask" "cask"
 brew_install "Transmission" "transmission" "caskroom/cask" "cask"
 brew_install "Unarchiver" "the-unarchiver" "caskroom/cask" "cask"
