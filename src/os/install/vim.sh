96d5ed0e23e9f1a1e17471974efe06f06f4bc00f
Cătălin Mariș <contact@catalinmaris.com>
2018-11-27 00:24:10 -0800

    🔧 Use minpac to manage Vim plugins
    Fix: #54

--- src/os/install/vim.sh
+++ src/os/install/vim.sh
@@ -7,37 +7,21 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 install_plugins() {
 
-    declare -r VUNDLE_DIR="$HOME/.vim/plugins/Vundle.vim"
-    declare -r VUNDLE_GIT_REPO_URL="https://github.com/VundleVim/Vundle.vim.git"
+    declare -r VIM_PACK_DIR="$HOME/.vim/pack"
+    declare -r MINPAC_DIR="$VIM_PACK_DIR/minpac/opt/minpac"
+    declare -r MINPAC_GIT_REPO_URL="https://github.com/k-takata/minpac.git"
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     # Install plugins.
 
     execute \
-        "rm -rf '$VUNDLE_DIR' \
-            && git clone --quiet '$VUNDLE_GIT_REPO_URL' '$VUNDLE_DIR' \
-            && printf '\n' | vim +PluginInstall +qall" \
+        "rm -rf $VIM_PACK_DIR \
+            && git clone --quiet $MINPAC_GIT_REPO_URL $MINPAC_DIR" \
         "Install plugins" \
         || return 1
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Install additional things required by some plugins.
-
-    execute \
-        ". $HOME/.bash.local \
-            && cd $HOME/.vim/plugins/tern_for_vim \
-            && npm install" \
-        "Install plugins (extra installs for 'tern_for_vim')"
-
-}
-
-update_plugins() {
-
-    execute \
-        "vim +PluginUpdate +qall" \
-        "Update plugins"
+    vim +PluginsSetup
 
 }
 
@@ -48,11 +32,7 @@ main() {
     print_in_purple "\n   Vim\n\n"
 
     "./$(get_os)/vim.sh"
-
-    printf "\n"
-
     install_plugins
-    update_plugins
 
 }
 
