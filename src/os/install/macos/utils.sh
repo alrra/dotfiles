75f0ae67ff40017a406367e00eb6eb6a3301cd5c
Cătălin Mariș <alrraa@gmail.com>
2019-01-22 13:58:20 -0800

    ✨ Make Homebrew automatically run `brew cleanup`
    Make `Homebrew` run `brew cleanup` periodically and for trigger
    individual formula cleanup on reinstall, install or upgrade.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Ref: https://brew.sh/2019/01/09/homebrew-1.9.0/

--- src/os/install/macos/utils.sh
+++ src/os/install/macos/utils.sh
@@ -5,20 +5,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-brew_cleanup() {
-
-    # By default Homebrew does not uninstall older versions
-    # of formulas so, in order to remove them, `brew cleanup`
-    # needs to be used.
-    #
-    # https://github.com/Homebrew/brew/blob/496fff643f352b0943095e2b96dbc5e0f565db61/share/doc/homebrew/FAQ.md#how-do-i-uninstall-old-versions-of-a-formula
-
-    execute \
-        "brew cleanup" \
-        "Homebrew (cleanup)"
-
-}
-
 brew_install() {
 
     declare -r CMD="$4"
