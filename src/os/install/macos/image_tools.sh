6414c88c277f0adc5eaf5dc481bf24b29308964b
Cătălin Mariș <alrraa@gmail.com>
2019-03-13 18:45:30 -0700

    🍎 Remove options from Homebrew installs
    Homebrew has remove all options from Homebrew/homebrew-core formulae.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    See also:
    
     * https://github.com/Homebrew/homebrew-core/pull/34421
     * https://github.com/Homebrew/homebrew-core/pull/36079
     * https://github.com/Homebrew/homebrew-core/issues/31510

--- src/os/install/macos/image_tools.sh
+++ src/os/install/macos/image_tools.sh
@@ -10,6 +10,6 @@ print_in_purple "\n   Image Tools\n\n"
 
 brew_install "GIMP" "lisanet-gimp" "caskroom/cask" "cask"
 brew_install "ImageAlpha" "imagealpha" "caskroom/cask" "cask"
-brew_install "ImageMagick" "imagemagick --with-webp"
+brew_install "ImageMagick" "imagemagick"
 brew_install "ImageOptim" "imageoptim" "caskroom/cask" "cask"
 brew_install "LICEcap" "licecap" "caskroom/cask" "cask"
