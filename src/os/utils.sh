a2aa34eb877843217c231415ac8c3684f08a12f0
Lison Foalr <lisonfoalr@protonmail.ch>
2019-04-08 17:34:34 +0800

    [refactor] move verify_os() from setup.sh to utils.sh

--- src/os/utils.sh
+++ src/os/utils.sh
@@ -349,3 +349,53 @@ show_spinner() {
     done
 
 }
+
+verify_os() {
+
+    declare -r MINIMUM_MACOS_VERSION="10.10"
+    declare -r MINIMUM_UBUNTU_VERSION="14.04"
+
+    local os_name=""
+    local os_version=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Check if the OS is `macOS` and
+    # it's above the required version.
+
+    os_name="$(uname -s)"
+
+    if [ "$os_name" == "Darwin" ]; then
+
+        os_version="$(sw_vers -productVersion)"
+
+        if is_supported_version "$os_version" "$MINIMUM_MACOS_VERSION"; then
+            return 0
+        else
+            printf "Sorry, this script is intended only for macOS %s+" "$MINIMUM_MACOS_VERSION"
+        fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Check if the OS is `Ubuntu` and
+    # it's above the required version.
+
+    elif [ "$os_name" == "Linux" ] && [ -e "/etc/lsb-release" ]; then
+
+        os_version="$(lsb_release -r | cut -f2)"
+
+        if is_supported_version "$os_version" "$MINIMUM_UBUNTU_VERSION"; then
+            return 0
+        else
+            printf "Sorry, this script is intended only for Ubuntu %s+" "$MINIMUM_UBUNTU_VERSION"
+        fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    else
+        printf "Sorry, this script is intended only for macOS and Ubuntu!"
+    fi
+
+    return 1
+
+}
