6f0938ca05b5dd2b396583cc27028b798487b368
Cătălin Mariș <alrraa@gmail.com>
2019-04-21 16:16:21 -0700

    🚮 Remove Ubuntu preferences that no longer work

--- src/os/preferences/ubuntu/ui_and_ux.sh
+++ src/os/preferences/ubuntu/ui_and_ux.sh
@@ -7,28 +7,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   UI & UX\n\n"
 
-execute "gsettings set com.canonical.indicator.bluetooth visible false" \
-    "Hide bluetooth icon from the menu bar"
-
-execute "gsettings set com.canonical.indicator.sound visible false" \
-    "Hide volume icon from the menu bar"
-
-execute "gsettings set com.canonical.indicator.power icon-policy 'charge' && \
-         gsettings set com.canonical.indicator.power show-time false" \
-    "Hide battery icon from the menu bar when the battery is not in use"
-
-execute "gsettings set com.canonical.indicator.datetime custom-time-format '%l:%M %p' && \
-         gsettings set com.canonical.indicator.datetime time-format 'custom'" \
-    "Use custom date format in the menu bar"
-
 execute "gsettings set org.gnome.desktop.background picture-options 'stretched'" \
     "Set desktop background image options"
 
 execute "gsettings set org.gnome.libgnomekbd.keyboard layouts \"[ 'us', 'ro' ]\"" \
     "Set keyboard languages"
-
-execute "gsettings set com.canonical.Unity.Launcher favorites \"[
-            'ubiquity-gtkui.desktop',
-            'nautilus-home.desktop'
-         ]\"" \
-    "Set Launcher favorites"
