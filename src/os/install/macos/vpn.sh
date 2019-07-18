b988f163956affac3108740a4c7d5ee9ffd31e15
Mak Abdennabi <hello@makabde.me>
2019-07-18 17:30:36 +0200

    Add NordVPN installer

---
+++ src/os/install/macos/vpn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   VPN\n\n"
+
+brew_install "NordVPN" "nordvpn" "caskroom/cask" "cask"
