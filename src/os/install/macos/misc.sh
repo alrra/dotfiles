9dafcdd2f965acac8d3c06c7a18b015bacb1335d
Cătălin Mariș <contact@catalinmaris.com>
2019-10-05 10:45:12 -0700

    🐛 Fix "caskroom/cask was moved" errors
    Use `homebrew/cask` instead of `caskroom/cask` because:
    
      " Error: caskroom/cask was moved. Tap homebrew/cask-cask instead. "

--- src/os/install/macos/misc.sh
+++ src/os/install/macos/misc.sh
@@ -8,9 +8,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Miscellaneous\n\n"
 
-brew_install "Android File Transfer" "android-file-transfer" "caskroom/cask" "cask"
+brew_install "Android File Transfer" "android-file-transfer" "homebrew/cask" "cask"
 brew_install "eyeD3" "eye-d3"
-brew_install "Spectacle" "spectacle" "caskroom/cask" "cask"
-brew_install "Transmission" "transmission" "caskroom/cask" "cask"
-brew_install "Unarchiver" "the-unarchiver" "caskroom/cask" "cask"
-brew_install "VLC" "vlc" "caskroom/cask" "cask"
+brew_install "Spectacle" "spectacle" "homebrew/cask" "cask"
+brew_install "Transmission" "transmission" "homebrew/cask" "cask"
+brew_install "Unarchiver" "the-unarchiver" "homebrew/cask" "cask"
+brew_install "VLC" "vlc" "homebrew/cask" "cask"
--- src/os/install/macos/quick_look.sh
+++ src/os/install/macos/quick_look.sh
@@ -8,5 +8,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Quick Look\n\n"
 
-brew_install "JSON" "quicklook-json" "caskroom/cask" "cask"
-brew_install "Markdown" "qlmarkdown" "caskroom/cask" "cask"
+brew_install "JSON" "quicklook-json" "homebrew/cask" "cask"
+brew_install "Markdown" "qlmarkdown" "homebrew/cask" "cask"
