ad91762543e1c02629433c4c7c1bc591b4b352fb
Gabriel Freiberg <gabefreiberg@gmail.com>
2021-05-21 09:26:19 -0600

    Fixes browser casks with required tap

--- src/os/install/macos/browsers.sh
+++ src/os/install/macos/browsers.sh
@@ -13,16 +13,16 @@ print_in_purple "\n   Browsers\n"
 printf "\n"
 
 brew_install "Chrome" "google-chrome" "--cask"
-brew_install "Chrome Canary" "google-chrome-canary" "--cask"
-brew_install "Chromium" "chromium" "--cask"
+brew_install "Chrome Canary" "google-chrome-canary" "--cask" "homebrew/cask-versions"
+brew_install "Chromium" "chromium" "--cask" "homebrew/cask-versions"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 printf "\n"
 
 brew_install "Firefox" "firefox" "--cask"
-brew_install "Firefox Developer" "firefox-developer-edition" "--cask"
-brew_install "Firefox Nightly" "firefox-nightly" "--cask"
+brew_install "Firefox Developer" "firefox-developer-edition" "--cask" "homebrew/cask-versions"
+brew_install "Firefox Nightly" "firefox-nightly" "--cask" "homebrew/cask-versions"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -31,5 +31,5 @@ brew_install "Firefox Nightly" "firefox-nightly" "--cask"
 
 if is_supported_version "$(get_os_version)" "10.11.4"; then
     printf "\n"
-    brew_install "Safari Technology Preview" "safari-technology-preview" "--cask"
+    brew_install "Safari Technology Preview" "safari-technology-preview" "--cask" "homebrew/cask-versions"
 fi
