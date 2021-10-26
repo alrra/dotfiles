0ab3090ac83ffebd6c413cc7a8cb8e805b9d30ea
Cătălin Mariș <contact@catalinmaris.com>
2021-10-25 21:17:13 -0700

    💥 Use Rectangle instead of Spectacle for window management on macOS
    From https://github.com/eczarny/spectacle/tree/e75c341ec2cba179c1bb8aa726a870c4132207df#important-note:
    
      " This project is not being actively maintained...
    
        Spectacle users have recommended Rectangle as
        an open source alternative. "

--- src/os/installs/macos/misc.sh
+++ src/os/installs/macos/misc.sh
@@ -10,7 +10,7 @@ print_in_purple "\n   Miscellaneous\n\n"
 
 brew_install "Android File Transfer" "android-file-transfer" "--cask"
 brew_install "eyeD3" "eye-d3"
-brew_install "Spectacle" "spectacle" "--cask"
+brew_install "Rectangle" "rectangle" "--cask"
 brew_install "Transmission" "transmission" "--cask"
 brew_install "Unarchiver" "the-unarchiver" "--cask"
 brew_install "VLC" "vlc" "--cask"
