17962ba90f7bf385a3ed604dca9ff1afff0b4ebf
utsavdahiya <utsav.163dahiya@gmail.com>
2021-10-31 14:49:10 +0530

    Update setup.sh

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -r GITHUB_REPOSITORY="alrra/dotfiles"
+declare -r GITHUB_REPOSITORY="utsavdahiya/dotfiles"
 
 declare -r DOTFILES_ORIGIN="git@github.com:$GITHUB_REPOSITORY.git"
 declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/main"
