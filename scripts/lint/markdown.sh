cb3eb9d22a3ce9f9f47ac591632b7ff200f9d5bb
Cătălin Mariș <contact@catalinmaris.com>
2023-12-22 17:53:13 +0200

    ⬆️  Update `markdownlint-cli` to `v0.37`

--- scripts/lint/markdown.sh
+++ scripts/lint/markdown.sh
@@ -12,7 +12,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../.." \
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 lint() (
-    npx markdownlint-cli@^0.32 "$1"
+    npx markdownlint-cli@^0.37 "$1"
 )
 
 check_markdown_files \
