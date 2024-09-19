b8469efc792a330000ce928226942bc42d63f74a
Mark Fleschler <octobeard@users.noreply.github.com>
2024-09-18 22:32:36 -0700

    Update install_ohmyzsh_p10k.sh

--- src/os/install_ohmyzsh_p10k.sh
+++ src/os/install_ohmyzsh_p10k.sh
@@ -7,3 +7,5 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 echo "Cloning p10k plugin for zsh"
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+p10k configure
