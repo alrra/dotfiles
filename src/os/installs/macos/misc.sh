b0cf9102c09ee3f8875cdb58475f7b6b4c0fadde
Cătălin Mariș <contact@catalinmaris.com>
2024-10-12 23:29:58 -0700

    💥 Remove Android File Transfer

--- src/os/installs/macos/misc.sh
+++ src/os/installs/macos/misc.sh
@@ -8,7 +8,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Miscellaneous\n\n"
 
-brew_install "Android File Transfer" "android-file-transfer" "--cask"
 brew_install "Rectangle" "rectangle" "--cask"
 brew_install "Unarchiver" "the-unarchiver" "--cask"
 brew_install "VLC" "vlc" "--cask"
