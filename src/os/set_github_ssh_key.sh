19d8c8ab449196bcc57161229c8f92fdb69fda46
Cătălin Mariș <contact@catalinmaris.com>
2026-04-12 23:03:08 -0700

    🔧 Use the recommended Ed25519 algorithm to generate the GitHub SSH key
    Ref: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key

--- src/os/set_github_ssh_key.sh
+++ src/os/set_github_ssh_key.sh
@@ -28,7 +28,7 @@ copy_public_ssh_key_to_clipboard () {
 
 generate_ssh_keys() {
     ask "Please provide an email address: " && printf "\n"
-    ssh-keygen -t rsa -b 4096 -C "$(get_answer)" -f "$1"
+    ssh-keygen -t ed25519 -C "$(get_answer)" -f "$1"
 
     print_result $? "Generate SSH keys"
 }
